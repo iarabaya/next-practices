71c48ab9b10ee7748c74b3fc7f53d07e6a8da2a4
Iara Evelyn Baya Vargas <iarabayav@gmail.com>
2025-05-06 01:06:16 -0300

    nuevos first steps next 13

---
+++ next-fherrera/01-first-steps/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Iara's home page",
+  description: "Generated with love by Vercel",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <h2>Hola de nuevo</h2>
+        {children}
+      </body>
+    </html>
+  );
+}
