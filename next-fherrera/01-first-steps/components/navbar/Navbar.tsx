ce9906172e36e807188395b85d47e996355dbd63
Iara Evelyn Baya Vargas <iarabayav@gmail.com>
2025-05-09 03:31:03 -0300

    feat: agrego links de next

--- next-fherrera/01-first-steps/components/navbar/Navbar.tsx
+++ next-fherrera/01-first-steps/components/navbar/Navbar.tsx
@@ -7,19 +7,32 @@
 //     })
 // }
 
-export const Navbar = async () => {
-    // await temporalAsync();
-    return ( 
-        <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-            
-            <span>Home</span>
+import Link from "next/link";
+import { HomeIcon } from "@primer/octicons-react";
 
-            <div className="flex flex-1"></div>
-            
-            <a className="mr-2" href="/about">About</a>
-            <a className="mr-2" href="/pricing">Pricing</a>
-            <a className="mr-2" href="/contact">Contact</a>
-       
-        </nav>
-    )
-}
+const navItems = [
+  { path: "/about", text: "About" },
+  { path: "/pricing", text: "Pricing" },
+  { path: "/contact", text: "Contact" },
+];
+
+export const Navbar = async () => {
+  // await temporalAsync();
+  return (
+    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+      <Link href={"/"} className="flex items-center">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
+
+			<div className="flex flex-1"></div>
+
+      {navItems.map((navItem) => (
+        <Link key={navItem.path} className="mr-2" href={navItem.path}>
+          {navItem.text}
+        </Link>
+      ))}
+			
+    </nav>
+  );
+};
