2b265c7f2cdaedd84bcade3ae4e83082326c6b86
Iara Evelyn Baya Vargas <iarabayav@gmail.com>
2025-05-22 02:11:48 -0300

    feat: add db connection and examples

--- next-linkedin/ecommerce-app/app/api/db.ts
+++ next-linkedin/ecommerce-app/app/api/db.ts
@@ -1,27 +1,30 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
 //DATABASE  CONNECTION LOGIC
+export async function connectToDb() {
+  const uri = process.env.DATABASE_URL || "";
 
-import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = process.env.DATABASE_URL || "";
+  //Create a MongoClient with a MongoClientOptions object to  set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
-//Create a MongoClient with a MongoClientOptions object to  set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
   try {
     //Connect the client to the server (optional starting v4.7)
-    await client.connect();   
+    await client.connect();
     //Send ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1});
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     //Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
 // run().catch(console.dir);
